d409424f3d2f368b403067d40b814f5641667b3d
KyrieJK <18910599692@163.com>
2020-03-15 01:20:30 +0800

    commit chapter02 习题2.1 complete

--- src/com/kyriejk/chapter02.scala
+++ src/com/kyriejk/chapter02.scala
@@ -1,5 +1,10 @@
 package com.kyriejk
 
 object chapter02 {
-  
+  /**
+   * 2.1 一个数字如果为正数，则signum为1；如果是负数，则signum为-1；如果是0，则signum为0
+   */
+  def signum(n:Int):Int={
+    if (n>0) 1 else if (n<0) -1 else 0
+  }
 }
